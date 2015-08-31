837439f4e25b0e43bfe85dd9a43bd96ca468c4a8
ernestyj@outlook.com <ernestyj@outlook.com>
2015-08-31 21:42:12 +0800

    c

--- hellobottle.py
+++ hellobottle.py
@@ -1,11 +1,18 @@
 import bottle
-from bottle import jinja2_template
+from bottle import jinja2_template, static_file
 
 app = bottle.Bottle()
+
 
 @app.route('/hello')
 def hello():
     # return jinja2_template('template/login.html')
     return jinja2_template('template/base.html')
+
+
+@app.route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='/static/' + filepath)
+
 
 app.run(host='localhost', port=81, debug=True)
