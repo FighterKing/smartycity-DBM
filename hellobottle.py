3ec67e9343b95969073c72063a47d9b4e8636dc2
ernestyj@outlook.com <ernestyj@outlook.com>
2015-08-31 20:12:33 +0800

    add base.html

--- hellobottle.py
+++ hellobottle.py
@@ -1,9 +1,11 @@
 import bottle
+from bottle import jinja2_template
 
 app = bottle.Bottle()
 
 @app.route('/hello')
 def hello():
-  return app.jinja2_template('template/admin/login.html')
+    # return jinja2_template('template/login.html')
+    return jinja2_template('template/base.html')
 
-app.run(host='localhost', port=8000, debug=True)
+app.run(host='localhost', port=81, debug=True)
