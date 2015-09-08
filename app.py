07db213b6b1648e99cf4f9f7260d49a82ce5d606
ernestyj@outlook.com <ernestyj@outlook.com>
2015-09-08 21:38:39 +0800

    change new templates

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import uuid
 import bottle
 import bottle_mysql
 
@@ -6,8 +5,9 @@ app = bottle.Bottle()
 app.config.load_config('app.conf')
 # dbhost: optional, default is localhost
 # keyword: The keyword argument name that triggers the plugin (default: ‘db’).
-plugin = bottle_mysql.Plugin(dbuser=app.config['mysql.user'], dbpass=app.config['mysql.pwd'], dbname=
-         app.config['mysql.dbname'], dbhost=app.config['mysql.host'], dictrows=['mysql.dictrows'] == 'True')
+plugin = bottle_mysql.Plugin(dbuser=app.config['mysql.user'], dbpass=app.config['mysql.pwd'],
+                             dbname=app.config['mysql.dbname'], dbhost=app.config['mysql.host'],
+                             dictrows=['mysql.dictrows'] == 'True')
 app.install(plugin)
 
 
@@ -17,9 +17,8 @@ app.install(plugin)
 #
 # Output : test page
 @app.route('/test')
-def hello():
-    # return bottle.jinja2_template('template/login.html')
-    return bottle.jinja2_template('template/index.html')
+def test():
+    return bottle.jinja2_template('template/base.html')
 
 
 # Description : get static files.
@@ -40,8 +39,7 @@ def server_static(filepath):
 @app.get('/')
 @app.get('/admin/login')
 def admin_login():
-    s_id = uuid.uuid1()
-    return bottle.jinja2_template('template/login.html', app_path='/admin/login', session_id=s_id)
+    return bottle.jinja2_template('template/login.html')
 
 
 # Description : login process.
@@ -53,13 +51,11 @@ def admin_login():
 def admin_login_process(db):
     username = bottle.request.forms.get('username')
     password = bottle.request.forms.get('password')
-	s_id = bottle.request.forms.get('s_id')
     db.execute('select password, user_type_id from user where username = "' + username + '"')
     row = db.fetchone()
     if row:
         if str(row[0]) == password and str(row[1]) == '4':
-            return admin_index(is_authenticated=True, request_session_original_user=True, current_username=username,
-                               user_has_usable_password=True, site_url='www.baidu.com')
+            return admin_index()
         else:
             return "<p>Your username or password is wrong, or you are not an administrator.</p>"
     else:
@@ -83,8 +79,7 @@ def admin_register():
 def admin_register_process(db):
     username = bottle.request.forms.get('username')
     password = bottle.request.forms.get('password')
-    status = db.execute('insert into user (username, password, user_type_id) values("'+ username + '","' + password + '", 4)')
+    status = db.execute('insert into user (username, password, user_type_id) values("'+ username + '", "' + password + '", 4)')
     return bottle.jinja2_template('template/login.html', app_path='/admin/login')
 
 app.run(host='localhost', port=1025, debug=True, reloader=True)
->>>>>>> 68338a0deb5fdf8c208f449a8a56e29ddd76baa5
