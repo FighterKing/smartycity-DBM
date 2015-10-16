3c98356e3cf6a9085ce9b76def2f9727bd635888
Kaiyang Lv <kevinlui598@gmail.com>
2015-10-16 14:05:47 +0800

    [API] update userrole api.

--- app.py
+++ app.py
@@ -181,14 +181,15 @@ def admin_user_update(userid, db):
 def admin_userrole_update(user_role_id, db):
     user_id = bottle.request.forms.get('user_id')
     role_id = bottle.request.forms.get('role_id')
-    description = bottle.request.forms.get('description')
+    role_description = bottle.request.forms.get('role_description')
+    user_role_description = bottle.request.forms.get('user_role_description')
     description_detail = bottle.request.forms.get('description_detail')
     role_type_id = bottle.request.forms.get('role_type_id')
 
     vdbm = dbm.DbM(db)
-    vdbm.update(table='role', condition=' where role_id=' + role_id,  role_type_id=role_type_id)
+    vdbm.update(table='role', condition=' where role_id=' + role_id,  role_type_id=role_type_id, description=role_description)
     vdbm.update(table='user_role', condition=' where user_role_id=' + user_role_id,  role_id=role_id,
-                description=description, description_detail=description_detail)
+                description=description_detail, description_detail=description_detail)
 
 
 def check():
